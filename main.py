007d58de79ea0f89a8d7ae47438e04dd495e57d4
HiddenExe <161987966+hiddenexee@users.noreply.github.com>
2024-03-24 21:58:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Discord: https://discord.com/users/1213658859185381387\nGmail: hidden4xe@gmail.com")
+print("""
+
+Discord: https://discord.com/users/1213658859185381387\nGmail: hidden4xe@gmail.com
+Telegram: @hiddenexe
+Gmail: hidden4xe@gmail.com
+
+""")
